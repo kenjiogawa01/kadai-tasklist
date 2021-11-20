95054106b45a647a4b363d719296e203abece5e8
k.ogawa <hakenyoukenji@gmail.com>
2021-11-20 13:46:39 +0000

    kadai_complete

--- database/migrations/2021_11_19_210356_create_tasks_table.php
+++ database/migrations/2021_11_19_210356_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTaskstTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
