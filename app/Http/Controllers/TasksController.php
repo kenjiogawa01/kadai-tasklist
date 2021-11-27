1e50a7d7e40a6079479f787f289cb11814cc4da0
k.ogawa <hakenyoukenji@gmail.com>
2021-11-27 06:33:49 +0000

    migration_fix

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -52,6 +52,7 @@ class TasksController extends Controller
         $task = new Task;
         $task->status = $request->status;
         $task->content = $request->content;
+        $task->user_id = $request->id;
         $task->save();
 
         return redirect('/');
