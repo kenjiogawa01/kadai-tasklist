0b3e316c9d633346ffe1a53e24d7126ca3417be7
k.ogawa <hakenyoukenji@gmail.com>
2021-11-27 06:28:28 +0000

    migration_fix

--- database/migrations/2021_11_26_234307_add_userid_to_tasks_table.php
+++ database/migrations/2021_11_26_234307_add_userid_to_tasks_table.php
@@ -14,6 +14,8 @@ class AddUseridToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
+            
+            $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
             
             $table->foreign('user_id')->references('id')->on('users');
